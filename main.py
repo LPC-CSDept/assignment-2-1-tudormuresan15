ff0abc0e75adb9363d541b88db49eb638863b42a
tudormuresan15 <136919518+tudormuresan15@users.noreply.github.com>
2023-06-25 23:04:44 +0300

    send file to assignment 1:

--- main.py
+++ main.py
@@ -16,3 +16,20 @@ def main():
 
 if __name__ == '__main__':
     main()
+    40+60
+    per=input()
+    per=int(per)
+    num=input()
+    num=int(num)
+    res=(per/100)*num
+    print(res)
+
+    m_num = int(input("40"))
+    f_num = int(input("60"))
+    total_num_students = m_num + f_num
+    m_perc = m_num / total_num_students
+    f_perc = f_num / total_num_students
+
+    print('m_num =', format(m_perc,'.0%'))
+    print(f_num =', format(f_perc,.0%))')
+
